d29948fc8f52adb4d44551728c236b712de5e7f6
Snoupax <snowpy1544@gmail.com>
2022-02-05 18:51:36 +0100

    new version with changing cards

--- js/drawHand.js
+++ js/drawHand.js
@@ -45,7 +45,7 @@ async function drawHand(){
   // Generation de 5 nombres aléatoires sans doublons.
   let randomNumbers = [];
 
-  for ( i = 0; i < 25; i++) {
+  for ( i = 0; i < 10; i++) {
     let randomNumber;
 
     do {
@@ -53,7 +53,9 @@ async function drawHand(){
     } while (randomNumbers.indexOf(randomNumber) !== -1);
  
     randomNumbers.push(randomNumber);
-}
+    console.log(randomNumbers[i]);
+    hand[i] = deck[randomNumbers[i]];
+  }
 
 
 
@@ -100,10 +102,13 @@ async function drawHand(){
 
     
     console.log(randomNumbers[i]);
-    hand[i] = deck[randomNumbers[i]];
-    imgFront.setAttribute("src", "css/Poker/cards/"+deck[randomNumbers[i]]+".png");
+    
+    imgFront.setAttribute("src", "css/Poker/cards/"+hand[i]+".png");
     imgBack.setAttribute("src", "css/Poker/cards/CardBack.png")
   }
+
+  const alert = document.getElementById("alert");
+  alert.innerHTML = "Cliques sur les cartes pour les retourner !";
 
 }
 
@@ -130,13 +135,15 @@ less.addEventListener('click', () => {
 })
 
 
-// Fonction pour retourner les cartes
-
+// Fonction pause (Pour mettre du delai à utiliser sur function async)
 function pause(ms) 
 {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+
+
+// Fonction qui actionne le retournement des cartes.
 async function flip (){
   
   for(i=0; i<5 ; i++){
@@ -154,6 +161,123 @@ async function flip (){
     }
 
   }
+  const alert = document.getElementById("alert");
+  alert.innerHTML = "Retourner les cartes que vous voulez changer avant de cliquer sur le bouton 'Changer les cartes'";
   
 }
 
+
+// Fonction d'affichage du résultat apres le retournement automatique des cartes 
+
+const displayComb = document.getElementById("flip");
+displayComb.addEventListener('click', async () => {
+    await pause(2500);
+    const displayCombi = document.getElementById("displayCombi");
+    displayCombi.innerHTML = ""
+    let combinaison = hand[0] + hand[1] + hand[2] + hand[3] + hand[4];
+    console.log(combinaison);
+    analyseHand(combinaison);
+
+    evaluateHand();
+})
+
+
+
+// Fonction qui permet à l'utilisateur de retourner les cartes lui même et afficher le resultat
+
+document.getElementById("board").addEventListener('click', () => {
+  let u = 0;
+    for(i=0;i<5;i++){
+      let card = document.getElementById("card"+ (i+1));
+      
+      if((card.checked == false)){
+        u++;
+      } else {
+        break;
+      }
+
+      
+  }
+  if(u==5){
+    const displayCombi = document.getElementById("displayCombi");
+    displayCombi.innerHTML = ""
+    let combinaison = hand[0] + hand[1] + hand[2] + hand[3] + hand[4];
+    console.log(combinaison);
+    analyseHand(combinaison);
+
+    evaluateHand();
+    const alert = document.getElementById("alert");
+    alert.innerHTML = "Retourner les cartes que vous voulez changer avant de cliquer sur le bouton 'Changer les cartes'";
+  }
+  
+})
+
+
+// Fonction qui va changer les cartes que l'utilisateur a retourné. 
+
+/* Verifie quel carte sont à Changer 
+
+Ensuite récuperer les cartes de remplacements dans hand[]
+
+Tirer de nouveau nombre aléatoire différents des premiers.
+
+préparer les nouvelles cartes à ajouter
+
+retirer les cartes retourner
+
+ajouter les nouvelle carte */
+
+async function change(){
+
+  for(i=0;i<5;i++){
+    let card = document.getElementById("card"+ (i+1));
+    
+    if((card.checked == true)){
+      await pause(50);
+      const cards = document.getElementsByClassName("cardSite");
+      cards[i].innerHTML = "";
+
+
+      const input = document.createElement("input");
+      input.type = "checkbox";
+      input.setAttribute("id", "card"+(i+1))
+      input.classList.add("card-flip");
+      input.setAttribute('checked',true);
+      const label = document.createElement("label");
+      label.setAttribute("for", 'card'+(i+1));
+      label.classList.add("label-flip");
+      const flipCard = document.createElement("div");
+      flipCard.classList.add("flip-card-inner");
+      const frontCard= document.createElement("div");
+      frontCard.classList.add("flip-card-front");
+      const backCard = document.createElement("div");
+      backCard.classList.add("flip-card-back")
+      const imgFront = document.createElement("img");
+      const imgBack = document.createElement("img");
+
+      
+      backCard.appendChild(imgBack);
+      frontCard.appendChild(imgFront);
+      flipCard.appendChild(frontCard);
+      flipCard.appendChild(backCard);
+      label.appendChild(flipCard);
+      cards[i].appendChild(input);
+      cards[i].appendChild(label);
+      
+      hand[i] = hand[(i+5)];
+
+    
+      console.log(hand[i]);
+      
+      imgFront.setAttribute("src", "css/Poker/cards/"+hand[i]+".png");
+      imgBack.setAttribute("src", "css/Poker/cards/CardBack.png")
+    } 
+
+
+
+  }
+  const displayCombi = document.getElementById("displayCombi");
+  displayCombi.innerHTML = "Tirage"
+  const alert = document.getElementById("alert");
+  alert.innerHTML = "Retourner les cartes pour voir le resultat";
+}
